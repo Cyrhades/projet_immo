34b89b797977869dec7e082fc35cc33ff7234b6e
Cyrhades <cyrhades76@gmail.com>
2023-11-17 10:14:47 +0100

    Création de l'interface du site

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,6 @@
 module.exports = (app) => {
-
     app.get('/', (request, response) => {
         let objController = require('../src/controllers/HomeController.js')
         objController.index(request, response)
     })
-
 };
